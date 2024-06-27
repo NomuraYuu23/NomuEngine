84e37f29e372fe4a46534e22be519f38fc0ba2cf
野村優羽 <@MSI>
2024-06-27 13:02:58 +0900

    中断

--- Project/Application/Object/Sample/SampleRigidBodyObject.cpp
+++ Project/Application/Object/Sample/SampleRigidBodyObject.cpp
@@ -21,18 +21,18 @@ void SampleRigidBodyObject::Initialize(Model* model)
 
 	rigidBody_.centerOfGravityVelocity = { 0.0f,0.0f,0.0f }; // 重心位置速度
 	// 重心位置 
-	rigidBody_.centerOfGravity = worldTransform_.GetWorldPosition() + Vector3{ 1.0f, 1.0f, 1.0f };
+	rigidBody_.centerOfGravity = worldTransform_.GetWorldPosition() + Vector3{ 0.0f, 1.0f, 1.0f };
 
 	// 力を入れる
-	const Vector3 pointOfAction = worldTransform_.GetWorldPosition() + Vector3{ -1.0f, -1.0f, -1.0f };
-	const Vector3 force = { 0.0f,0.0f,150.0f };
+	const Vector3 pointOfAction = worldTransform_.GetWorldPosition() + Vector3{ 0.0f, -1.0f, -1.0f };
+	const Vector3 force = { 0.0f,0.0f, 500.0f };
 	rigidBody_.torque = RigidBody::TorqueCalc(rigidBody_.centerOfGravity, pointOfAction, force);
 
 	// 慣性テンソル作成
-	rigidBody_.inertiaTensor = InertiaTensor::CreateRectangular(1.0f, Vector3{ 2.0f, 2.0f, 2.0f });
+	rigidBody_.inertiaTensor = InertiaTensor::CreateRectangular(0.01f, Vector3{ 2.0f, 2.0f, 2.0f });
 
 	// 基本姿勢での慣性テンソル作成
-	rigidBody_.basicPostureInertiaTensor = InertiaTensor::CreateRectangular(1.0f, Vector3{ 2.0f, 2.0f, 2.0f });
+	rigidBody_.basicPostureInertiaTensor = InertiaTensor::CreateRectangular(0.01f, Vector3{ 2.0f, 2.0f, 2.0f });
 	
 	// 姿勢行列作成
 	rigidBody_.postureMatrix = Matrix4x4::MakeRotateXYZMatrix({ 0.0f,0.0f,0.0f });
@@ -45,6 +45,17 @@ void SampleRigidBodyObject::Initialize(Model* model)
 void SampleRigidBodyObject::Update()
 {
 
+	// 速度算出
+	Vector3 velocity = RigidBody::PointVelocityCalc(
+		rigidBody_.angularVelocity,
+		rigidBody_.centerOfGravityVelocity,
+		worldTransform_.GetWorldPosition(),
+		rigidBody_.centerOfGravity
+	);
+
+	// 速度更新
+	worldTransform_.transform_.translate += velocity * kDeltaTime_;
+
 	// 姿勢行列を更新
 	rigidBody_.postureMatrix =  RigidBody::PostureCalc(rigidBody_.postureMatrix, rigidBody_.angularVelocity, kDeltaTime_);
 
@@ -54,22 +65,11 @@ void SampleRigidBodyObject::Update()
 	// 角運動量を更新
 	rigidBody_.angularMomentum = RigidBody::AngularMomentumCalc(rigidBody_.angularMomentum, rigidBody_.torque, kDeltaTime_);
 
-	// 速度算出
-	Vector3 velocity = RigidBody::PointVelocityCalc(
-		rigidBody_.angularVelocity,
-		rigidBody_.centerOfGravityVelocity,
-		worldTransform_.GetWorldPosition(),
-		rigidBody_.centerOfGravity
-	);
-
 	// 角速度を更新
 	rigidBody_.angularVelocity = RigidBody::AngularVelocityCalc(rigidBody_.inertiaTensor, rigidBody_.angularMomentum);
 
 	// ひねり力を0に
 	rigidBody_.torque = { 0.0f,0.0f,0.0f };
-
-	// 速度更新
-	worldTransform_.transform_.translate += velocity * kDeltaTime_;
 
 	// ワールドトランスフォーム更新
 	worldTransform_.UpdateMatrix(rigidBody_.postureMatrix);
@@ -94,6 +94,11 @@ void SampleRigidBodyObject::ImGuiDraw()
 
 	ImGui::Begin("SampleRigidBodyObject");
 	ImGui::Text("angularMomentum X::%f, Y::%f, Z::%f", rigidBody_.angularMomentum.x, rigidBody_.angularMomentum.y, rigidBody_.angularMomentum.z);
+	ImGui::Text("angularVelocity X::%f, Y::%f, Z::%f", rigidBody_.angularVelocity.x, rigidBody_.angularVelocity.y, rigidBody_.angularVelocity.z);
+	
+	ImGui::Text("postureMatrix0 X::%f, Y::%f, Z::%f", rigidBody_.postureMatrix.m[0][0], rigidBody_.postureMatrix.m[0][1], rigidBody_.postureMatrix.m[0][2]);
+	ImGui::Text("postureMatrix1 X::%f, Y::%f, Z::%f", rigidBody_.postureMatrix.m[1][0], rigidBody_.postureMatrix.m[1][1], rigidBody_.postureMatrix.m[1][2]);
+	ImGui::Text("postureMatrix2 X::%f, Y::%f, Z::%f", rigidBody_.postureMatrix.m[2][0], rigidBody_.postureMatrix.m[2][1], rigidBody_.postureMatrix.m[2][2]);
 	ImGui::End();
 
 
